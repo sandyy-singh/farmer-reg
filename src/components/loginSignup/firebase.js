6a12baee58b7c345a4a5392e26912c0b6b11228c
sandyy-singh <sandeeprewa43@gmail.com>
2023-09-01 15:31:16 +0530

    added firebase project

--- src/components/loginSignup/firebase.js
+++ src/components/loginSignup/firebase.js
@@ -13,11 +13,26 @@ const firebaseConfig = {
   projectId: "dcdataapp",
   storageBucket: "dcdataapp.appspot.com",
   messagingSenderId: "1086430953312",
-  appId: "1:1086430953312:web:965bb556679dc411eb2805",
-  measurementId: "G-CH6EHMHTG9"
+  appId: "1:1086430953312:web:d32da2e5cef188baeb2805",
+  measurementId: "G-74GRFTPCD7"
 };
 
 // Initialize Firebase
 export const apppp = initializeApp(firebaseConfig);
 //  const analytics = getAnalytics(app);
  
+
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBlqUajoQSPMEaO2Z2rCAnfNYlcaLS-N_A",
+//   authDomain: "dcdataapp.firebaseapp.com",
+//   databaseURL: "https://dcdataapp-default-rtdb.firebaseio.com",
+//   projectId: "dcdataapp",
+//   storageBucket: "dcdataapp.appspot.com",
+//   messagingSenderId: "1086430953312",
+//   appId: "1:1086430953312:web:965bb556679dc411eb2805",
+//   measurementId: "G-CH6EHMHTG9"
+// };
